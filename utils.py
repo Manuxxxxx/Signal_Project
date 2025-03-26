af273adf32b88781b13aa3964c139b028dac13d9
Emanuele Ricca <emanuele.ricca.10@gmail.com>
2025-03-26 15:35:08 +0100

    fixed color loading error

--- utils.py
+++ utils.py
@@ -4,8 +4,9 @@ import os
 def load_images_from_folder(folder):
     images = []
     for filename in os.listdir(folder):
-        img = cv2.imread(os.path.join(folder,filename))
+        img = cv2.imread(os.path.join(folder,filename), cv2.IMREAD_COLOR)
         if img is not None:
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             images.append(img)
     return images
 
